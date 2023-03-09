33d4e1e30e4180a65ae05ad717c12f0c709a2888
Yogendra Manawat <81252768+Ymanawat@users.noreply.github.com>
2023-03-09 14:39:44 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          <code>Website in currently in development</code>
+          <code>Website is currently in development</code>
         </p>
         <a
           className="App-link"
